2b8a09a4e41a2a53ac64607baf5e92063f10ac48
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-04-03 17:58:53 +0500

    chef

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => (
     <Header />  
     <AboutUs />
     <SpecialMenu />
-    {/* <Chef /> */}
+    <Chef />
     {/* <Intro /> */}
     {/* <Laurels /> */}
     {/* <Gallery /> */}
