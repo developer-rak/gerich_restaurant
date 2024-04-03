cbf8d003a6d5fed2108980f25b7c3f16544039e0
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-04-03 15:24:32 +0500

    about and menu

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -10,7 +10,23 @@ const AboutUs = () => (
     </div>
 
     <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt="spoon__image" className='spoon__img' />
+        <p className="p__opensans">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
+        <button className='custom__button' type='button'>Know More</button>
+      </div>
 
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="knife__image" />
+      </div>
+
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={images.spoon} alt="spoon__image" className='spoon__img' />
+        <p className="p__opensans">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
+        <button className='custom__button' type='button'>Know More</button>
+      </div>
     </div>
   </div>
 );
